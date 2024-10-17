37b1f59b9eb2d7e5364e2a3e2001b4a05e79f659
William Miller <Wmilleiv@gmail.com>
2024-10-16 20:42:53 -0400

    8_6

--- practice.cpp
+++ practice.cpp
@@ -1,17 +1,52 @@
 #include <iostream>
+#include <exception>
 
-int accumulate(int x){
-	static int sum{0};
-	sum+=x;
-	return sum;
+int getInteger(){
+	std::cout << "Enter an integer: ";
+	int x{0};
+	std::cin >> x;
+
+	return x;
 }
 
+char getOperation(){
+	std::cout << "Enter an operator (+,-,*,/,%):";
+	char op{};
+	std::cin >> op;
+
+	return op;
+}
+int calculate(int x, int y, char op){
+	switch(op){
+	case '+':
+		return x+y;
+	case '-':
+		return x-y;
+	case '*':
+		return x*y;
+	case '/':
+		if(y==0){
+			throw 789;
+		}
+		return x/y;
+	case '%':
+		return x%y;
+	default:
+		throw 987;
+		return 0;
+	}
+}
 int main()
 {
-    std::cout << accumulate(4) << '\n'; // prints 4
-    std::cout << accumulate(3) << '\n'; // prints 7
-    std::cout << accumulate(2) << '\n'; // prints 9
-    std::cout << accumulate(1) << '\n'; // prints 10
+	int x{getInteger()};
+	int y{getInteger()};
 
+	char op{getOperation()};
+	
+	try{
+		std::cout << x << op << y <<'=' << calculate(x,y,op) << '\n';
+	}catch(int e){
+		std::cout << "Esception: " << e <<'\n';
+	}
     return 0;
 }
