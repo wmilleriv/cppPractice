d6e8d8ba12d725324cd34a10c09fbc478261d147
William Miller <Wmilleiv@gmail.com>
2024-10-18 20:31:08 -0400

    8_8Complete

--- practice.cpp
+++ practice.cpp
@@ -3,24 +3,30 @@
 int main()
 {
     // outer loops between 1 and 5
-    int outer{ 5 };
-    while (outer >=0)
+    int outer{ 1 };
+    while (outer <=5)
     {
         // For each iteration of the outer loop, the code in the body of the loop executes once
 
         // inner loops between 1 and outer
         // note: inner will be created and destroyed at the end of the block
-        int inner{outer };
+        int inner{5 };
         while (inner >= 1)
 		
         {
-            std::cout << inner << ' ';
+		if(outer < inner){
+            		std::cout  << ' ';
+		}
+		else{
+			std::cout << inner;
+		}
             --inner;
         }
+	std::cout << '\n';
 
         // print a newline at the end of each row
-        std::cout << '\n';
-        --outer;
+        ++outer;
+
     } // inner destroyed here
 
     return 0;
