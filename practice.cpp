b7aabfd793c6b38267a9523232212e621534f702
William Miller <Wmilleiv@gmail.com>
2024-10-18 20:02:47 -0400

    8_8

--- practice.cpp
+++ practice.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 
-int main(){
-	for(char c{'a'};c<='z';c++){
-		std::cout << c << " " << static_cast<int>(c) << '\n';
-	}
-	return 0;
+int main()
+{
+    // outer loops between 1 and 5
+    int outer{ 5 };
+    while (outer >=0)
+    {
+        // For each iteration of the outer loop, the code in the body of the loop executes once
+
+        // inner loops between 1 and outer
+        // note: inner will be created and destroyed at the end of the block
+        int inner{outer };
+        while (inner >= 1)
+		
+        {
+            std::cout << inner << ' ';
+            --inner;
+        }
+
+        // print a newline at the end of each row
+        std::cout << '\n';
+        --outer;
+    } // inner destroyed here
+
+    return 0;
 }
-
-
