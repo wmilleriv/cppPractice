795c7b18c3bc7e89b4915b2ca4eab4e263e2fdcb
William Miller <Wmilleiv@gmail.com>
2024-10-29 20:45:46 -0400

    chapter 11 done

--- practice.cpp
+++ practice.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 
-template <int N>
-constexpr int factorial()
-{
-    static_assert(N >= 0);
-
-    int product { 1 };
-    for (int i { 2 }; i <= N; ++i)
-        product *= i;
-
-    return product;
+template<typename T>
+T add( T x, T y){
+	return x+y;	
+}
+template<typename U, typename V>
+U mult(U x, V y){
+	return x*y;
+}
+template<typename W, typename Y>
+auto sub(W x, Y y){
+	return x-y;
 }
 
 int main()
 {
-    static_assert(factorial<0>() == 1);
-    static_assert(factorial<3>() == 6);
-    static_assert(factorial<5>() == 120);
-
-    //factorial<-3>(); // should fail to compile
-
-    return 0;
+	std::cout << add(2, 3) << '\n';
+	std::cout << add(1.2, 3.4) << '\n';
+	std::cout << mult(2, 3) << '\n';
+	std::cout << mult(1.2, 3) << '\n';
+	std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
+	return 0;
 }
