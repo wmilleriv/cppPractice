020a3f452d1ccd705fabd2a506bda4497e1e2e8b
William Miller <Wmilleiv@gmail.com>
2024-10-31 18:46:46 -0400

    13_3

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <algorithm>
-
+namespace Monster{ 
+	enum monsterType{
+		orc,
+		goblin,
+		troll,
+		ogre,
+		skeleton,
+	};
+}
 void sort2(int& x, int& y){
 	if(y>x){
 		std::swap(x,y);
@@ -11,7 +19,7 @@ int main()
 {
     int x { 7 };
     int y { 5 };
-
+	[[maybe_unused]]Monster::monsterType mon{Monster::troll};
     std::cout << x << ' ' << y << '\n'; // should print 7 5
 
     sort2(x, y); // make sure sort works when values need to be swapped
