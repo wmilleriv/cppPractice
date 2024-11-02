9a12a64340cc132630040d454585596b488c9837
William Miller <Wmilleiv@gmail.com>
2024-11-01 21:13:03 -0400

    13_10_1

--- practice.cpp
+++ practice.cpp
@@ -1,32 +1,18 @@
 #include <iostream>
-#include <algorithm>
-namespace Monster{ 
-	enum monsterType{
-		orc,
-		goblin,
-		troll,
-		ogre,
-		skeleton,
-	};
-}
-void sort2(int& x, int& y){
-	if(y>x){
-		std::swap(x,y);
-	}
+
+struct Advertisement{
+	int watched{0};
+	int clicked{0};
+	double earnings{0};
+};
+
+void printAdInfo(Advertisement ad){
+	std::cout << ad.watched << ad.clicked << ad.earnings;
 }
 
 int main()
 {
-    int x { 7 };
-    int y { 5 };
-	[[maybe_unused]]Monster::monsterType mon{Monster::troll};
-    std::cout << x << ' ' << y << '\n'; // should print 7 5
-
-    sort2(x, y); // make sure sort works when values need to be swapped
-    std::cout << x << ' ' << y << '\n'; // should print 5 7
-
-    sort2(x, y); // make sure sort works when values don't need to be swapped
-    std::cout << x << ' ' << y << '\n'; // should print 5 7
-
-    return 0;
+	Advertisement ad{3, 34, 15};
+	printAdInfo(ad);
+	return 0;
 }
