55bcfb6b148aa98c7cd181333a40608d96281d64
William Miller <Wmilleiv@gmail.com>
2024-11-06 20:18:05 -0500

    14_3 done

--- practice.cpp
+++ practice.cpp
@@ -10,6 +10,10 @@ struct IntPair{
 	void print(){
 		std::cout << "{" << x << ", " << y << "}";
 	}
+
+	bool isEqual(IntPair p){
+		return x==p.x && y==p.y;
+	}
 		
 
 	
@@ -26,6 +30,9 @@ int main()
 
 	std::cout << "p2: ";
 	p2.print();
+
+	std::cout << "p1 and p1 " << (p1.isEqual(p1) ? "are equal\n" : "are not equal\n");
+	std::cout << "p1 and p2 " << (p1.isEqual(p2) ? "are equal\n" : "are not equal\n");
 
 	return 0;
 }
