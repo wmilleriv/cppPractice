e0fd1be5f8176546f3ac82eb9769bc4b0a0e00bf
William Miller <Wmilleiv@gmail.com>
2024-11-09 21:12:19 -0500

    14 final 1_2

--- point.cpp
+++ point.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 class Point2d{
 	private:
 		double m_x{0.0};
@@ -12,17 +14,21 @@ class Point2d{
 		void print(){
 			std::cout << "<" << m_x << "," << m_y  <<">\n";
 		}
-};
+		double distanceTo(Point2d p){
+			return std::sqrt((p.m_x - m_x)*(p.m_x - m_x) + (p.m_y - m_y)*(p.m_y - m_y));
+	
+		}
+	};
 
 int main()
-{
+{   
     Point2d first{};
     Point2d second{ 3.0, 4.0 };
-
-    // Point2d third{ 4.0 }; // should error if uncommented
 
     first.print();
     second.print();
 
+    std::cout << "Distance between two points: " << first.distanceTo(second) << '\n';	
+    
     return 0;
 }
