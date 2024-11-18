29d6a7da2115ac75fc3c1563c923f9ec39c9a58b
William Miller <Wmilleiv@gmail.com>
2024-11-17 20:48:34 -0500

    15 final part 1

---
+++ monster.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+enum class monsterType{
+	dragon, 
+	goblin, 
+	ogre, 
+	orc, 
+	skeleton, 
+	troll, 
+	vampire,
+       	zombie,
+	maxMonsterTypes,
+};
+int main(){
+	return 0;
+}
