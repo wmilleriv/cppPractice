e13246305eb91ef7b8465eec3e5c5ba5788749e4
William Miller <Wmilleiv@gmail.com>
2024-11-18 18:23:42 -0500

    15 final 3

--- monster.cpp
+++ monster.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 
-enum class monsterType{
-	dragon, 
-	goblin, 
-	ogre, 
-	orc, 
-	skeleton, 
-	troll, 
-	vampire,
-       	zombie,
-	maxMonsterTypes,
+
+class Monster{
+	public:
+
+	enum Type{
+		dragon, 
+		goblin, 
+		ogre, 
+		orc, 
+		skeleton, 
+		troll, 
+		vampire,
+       		zombie,
+		maxMonsterTypes,
+	};
+	private:
+
+		Type m_type{maxMonsterTypes};
+		std::string m_name{"bitch boy"};
+		std::string m_roar{"uWuWuWuW"};
+		int m_hitPoints{100};
+	public:
+		Monster(Type t, std:string name, std::string roar,  int hp)
+			:m_type{t}, m_name{name}, m_roar{roar}, m_hitPoints{hp}
+		{}
+
 };
+
 int main(){
+	Monster skeleton{ Monster::skeleton, "Bones", "*rattle*", 4 };	
 	return 0;
 }
