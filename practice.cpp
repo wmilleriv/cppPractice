61935ecb3af65c4bd9c16d6686b2bf8efddbd546
William Miller <Wmilleiv@gmail.com>
2024-12-02 18:57:39 -0500

    16_4

--- practice.cpp
+++ practice.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <vector>
 
+// Write your printElement function here
 
-int main(){
+int main()
+{
+    std::vector v1 { 0, 1, 2, 3, 4 };
+    printElement(v1, 2);
+    printElement(v1, 5);
 
-	std::vector<char> hello{'h', 'e', 'l', 'l', '0'};
+    std::vector v2 { 1.1, 2.2, 3.3 };
+    printElement(v2, 0);
+    printElement(v2, -1);
 
-	std::cout << "The array has " << std::size(hello) << " characters\n";
-	std::cout << hello.at(1);
-	return 0;
+    return 0;
 }
