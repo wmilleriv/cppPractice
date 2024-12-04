7a120de0a7d98505eb25642e3b62dcafe10c8bb7
William Miller <Wmilleiv@gmail.com>
2024-12-04 17:33:12 -0500

    16_5_1

--- practice.cpp
+++ practice.cpp
@@ -5,7 +5,9 @@ int main()
 {
     std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
 
-    // Add your code here
+    for(int i{0};i<static_cast<int>(arr.size());i++){
+	    std::cout << arr[i] << '\n';
+    }
 
     return 0;
 }
