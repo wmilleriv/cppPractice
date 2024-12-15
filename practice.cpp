f68ad98e910b0a7d6b9dc42357764879b0ecf2c7
William Miller <Wmilleiv@gmail.com>
2024-12-15 18:23:54 -0500

    16_6_4 final

--- practice.cpp
+++ practice.cpp
@@ -1,51 +1,67 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 
 template <typename T>
-T getUserInput(){
-	T in{0};
-	std::cout << "Enter a value: ";
-	std::cin >> in;
-	return in;
-}
-
-template <typename T>
-bool isInArray(const std::vector<T> arr, T item)
+void printArray(const std::vector<T>& arr)
 {
-	for(int i{0};i<arr.size();i++){
-		if(arr[i]==item)
-			return true;
-	}
-	return false;
+    for (std::size_t index{ 0 }; index < arr.size(); ++index)
+    {
+        std::cout << arr[index] << ' ';
+    }
+
+    if (arr.size() > 0)
+        std::cout << '\n';
 }
 
 template <typename T>
-void printArray(const std::vector<T> arr){
-
-
-    for(int i{0};i<static_cast<T>(arr.size());i++){
-    	std::cout << arr[i] << '\n';
+int findIndex(const std::vector<T>& arr, T val)
+{
+    for (std::size_t index{ 0 }; index < arr.size(); ++index)
+    {
+        if (arr[index] == val)
+            return static_cast<int>(index);
     }
+
+    return -1; // -1 is not a valid index, so we can use it as an error return value
+}
+
+// Passing in low and high allows the compiler to infer the type of the input we want
+template <typename T>
+T getValidNumber(std::string_view prompt, T low, T high)
+{
+    // First, read in valid input from user
+    T val {};
+    do
+    {
+        std::cout << prompt;
+        std::cin >> val;
+
+        // if the user entered an invalid character
+        if (!std::cin)
+            std::cin.clear(); // reset any error flags
+
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any extra characters in the input buffer (regardless of whether we had an error or not)
+
+    } while (val < low || val > high);
+
+    return val;
 }
 
 int main()
 {
-    std::vector arr{ 4, 6, 7, 3, 8, 2, 1, 9 };
+    std::vector arr{ 4.4, 6.6, 7.7, 3.3, 8.8, 2.2, 1.1, 9.9 };
+
+    auto num { getValidNumber("Enter a number between 1 and 9: ", 1.0, 9.0) };
 
     printArray(arr);
 
-    twmplate <typename T>
-    T x{getUserInput()};
+    int index { findIndex(arr, num) };
 
-    if(isInArray(arr, x))
-	{
-		std::cout << "yes";
-	}
-	else
-	{
-	std::cout << "no";
-	}
-
+    if (index != -1)
+        std::cout << "The number " << num << " has index " << index << '\n';
+    else
+        std::cout << "The number " << num << " was not found\n";
 
     return 0;
 }
