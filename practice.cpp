92c528be162ca22b63e6aa7b9996a92a5ca814c2
William Miller <Wmilleiv@gmail.com>
2024-12-25 15:15:06 -0500

    fib

---
+++ practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector fibonacci { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 };
+
+    for (int num : fibonacci) // iterate over array fibonacci and copy each value into `num`
+       std::cout << num << ' '; // print the current value of `num`
+
+    std::cout << '\n';
+
+    return 0;
+}
