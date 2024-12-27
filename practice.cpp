1d51a46d22e090068d5be2dd5a5e3cf377f5a7fc
William Miller <Wmilleiv@gmail.com>
2024-12-27 14:31:11 -0500

    16.8 compiles

--- practice.cpp
+++ practice.cpp
@@ -5,7 +5,7 @@ int main()
 {
     std::vector names { "Alex", "Betty", "Caoline", "Dave", "Emily", "Fred", "Greg", "Holly" };
 
-    for (auto num : nam) // iterate over array fibonacci and copy each value into `num`
+    for (auto num : names) // iterate over array fibonacci and copy each value into `num`
        std::cout << num << ' '; // print the current value of `num`
 
     std::cout << '\n';
