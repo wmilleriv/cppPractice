24a26e3a67b201c9bfd29697d784344fb554a698
William Miller <Wmilleiv@gmail.com>
2025-02-28 11:50:56 -0500

    pi calc

--- pi.cpp
+++ pi.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
+double k{1};
+double pi{0};
+
 int main(){
     for(int i{0};i<1000000;i++){
+        if(i%2 ==0){
+            pi+=4/k;
+        }else{
+            pi-=4/k;
+        }
+        k+=2;
     }
+    std::cout << pi << '\n';
     return 0;
 }
